4143061a481f9c4c39578a6ea697f66a840df0f4
dev-fahim <fahim6668@gmail.com>
2020-05-05 02:25:02 +0600

    About variable

---
+++ variable.py
@@ -0,0 +1,41 @@
+
+a = 12.3434534563463456
+
+print("%.2f" % (a, ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+integer_number = 12  # Integer
+float_number = 12.345  # Float
+string = "Fahim Al Islam"  # Sting
+boolean_True = True  # Boolean
+boolean_False = False  # Boolean
+
+"""
+print(type(integer_number))
+print(type(float_number))
+print(type(string))
+print(type(boolean_True))
+print(type(boolean_False))
+
+
+
+Python is dynamically typed programming language
+type is mutable not explicit
+"""
